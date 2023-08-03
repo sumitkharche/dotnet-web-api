584644136beb747fdddabbe937d382ef8c796483
Sumit Kharche <sumit.kharche@yahoo.in>
2023-08-03 09:43:14 +0000

    added config file for prod

--- demo-web-api/Controllers/WeatherForecastController.cs
+++ demo-web-api/Controllers/WeatherForecastController.cs
@@ -10,12 +10,16 @@ public class WeatherForecastController : ControllerBase
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
+    private readonly IConfiguration _configuration;
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IWebHostEnvironment _webHostEnvironment;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, 
+        IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
     {
+        _configuration = configuration;
         _logger = logger;
+        _webHostEnvironment = webHostEnvironment;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
@@ -25,8 +29,19 @@ public class WeatherForecastController : ControllerBase
         {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+            DB = _configuration.GetValue<string>("Dbval")
         })
         .ToArray();
     }
+
+    //[HttpGet(Name = "GetConfig")]
+    //public IActionResult GetConfig()
+    //{
+      
+    //    //string value = _configuration.GetValue<string>("Dbval");
+    //    return Ok();
+    //}
+
+
 }
